3e83bb93212c98a117a9c3dae8cd31b98d29156e
diademist <vladimir.rastopchin@gmail.com>
2023-10-21 19:52:52 +0300

    Update settings.py
    Removed secret key

--- EBookshelf/booksite/booksite/settings.py
+++ EBookshelf/booksite/booksite/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m%kz!+fh57ymm75jv9@$*!b$%l7y)v4hwb8o(2xdk-6)3hmc_f'
+# SECURITY WARNING: keep the secret key used in production secret! Key removed, but can be found in GDrive. Place file to gitignore for the future. 
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
